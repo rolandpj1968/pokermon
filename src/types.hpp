d7d5645098846515184f0da0da1b3286ce086868
rolandp <rolandp@amazon.com>
2021-10-17 08:43:17 +0200

    Catch-up from long time ago

--- src/types.hpp
+++ src/types.hpp
@@ -113,7 +113,7 @@ namespace Poker {
   //
   //  - for a Pair, the hi u16 is the rank of the pair, and the low u16 is the rank-bits of the remaining (three) cards
   //  - for TwoPair the hi u16 is the rank of the high pair (hi u8) and the rank of the low pair (low u8) and the low u16 is the rank-bits of the kicker
-  //  - for a Set, the hi u16 is the rank of the set, and the low u16 is rank-bits of the remaining (two) cards
+  //  - for a Set,  the hi u16 is the rank of the set, and the low u16 is rank-bits of the remaining (two) cards
   //  - for a FullHouse the hi u16 is the rank of the set/trip and the low u16 is the rank of the pair
   //  - for FourOfAKind, the hi u16 is the rank of the pair, and the low u16 is the rank-bits of the kicker
   //  - for all other hands the high u16 is 0 and the low u16 is the rank-bits of all cards in the hand
