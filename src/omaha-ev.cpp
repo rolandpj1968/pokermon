9dbd7e3bd2caf9340e8632ef04df4e66221f25b9
rolandp <rolandp@amazon.com>
2023-07-13 16:39:49 +0200

    Threaded omaha-ev working - need to clean up...

--- src/omaha-ev.cpp
+++ src/omaha-ev.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <utility>
-#include <set>
+#include <thread>
 
 #include "dealer.hpp"
 #include "hand-eval.hpp"
@@ -39,21 +39,15 @@ static void add_action_and_value(std::pair<int, double>* action_and_value, int h
   entry.second += value;
 }
 
-int main() {
-  std::seed_seq seed{2, 3, 5, 7, 13};
+static void do_thread_stuff(const std::pair<int, int>& p) {
+  printf("Hallo from thread: %d %d\n", p.first, p.second);
+}
+
+static void evaluate_hands(int n_deals, double* p0_total_value, std::pair<int, double>* p0_action_and_value, double* p1_total_value, std::pair<int, double>* p1_action_and_value, int seed5) {
+  std::seed_seq seed{2, 3, 5, 7, seed5};
   Dealer::DealerT dealer(seed);
 
-  Normal::init_omaha_hold_normal_index();
-
-  const int N_DEALS = 1000000;
-
-  double p0_total_value = 0.0;
-  double p1_total_value = 0.0;
-
-  std::pair<int, double>* p0_action_and_value = new std::pair<int, double>[Normal::N_OMAHA_HOLE_NORMALS]();
-  std::pair<int, double>* p1_action_and_value = new std::pair<int, double>[Normal::N_OMAHA_HOLE_NORMALS]();
-
-  for(int deal_no = 0; deal_no < N_DEALS; deal_no++) {
+  for(int deal_no = 0; deal_no < n_deals; deal_no++) {
     auto cards = dealer.deal(4+4+3+1+1);
 
     auto p0_hole = std::make_tuple(CardT(cards[0+0]), CardT(cards[0+1]), CardT(cards[0+2]), CardT(cards[0+3]));
@@ -82,19 +76,123 @@ int main() {
       p1_hand_value = 1.0;
     }
 
-    p0_total_value += p0_hand_value;
-    p1_total_value += p1_hand_value;
+    *p0_total_value += p0_hand_value;
+    *p1_total_value += p1_hand_value;
 
     add_action_and_value(p0_action_and_value, p0_hole_normal_index, p0_hand_value);
     add_action_and_value(p1_action_and_value, p1_hole_normal_index, p1_hand_value);
   }
+}
 
-  printf("%d deals / p0 EV %+4.2lf / p1 EV %+4.2lf\n", N_DEALS, p0_total_value/N_DEALS, p1_total_value/N_DEALS);
+int main(int argc, char* argv[]) {
+  // auto my_p = std::make_pair(1, 2);
+  // std::thread my_thread (do_thread_stuff, my_p);
+  // my_thread.join();
+  
+  // std::seed_seq seed{2, 3, 5, 7, 13};
+  // Dealer::DealerT dealer(seed);
+
+  Normal::init_omaha_hold_normal_index();
+
+  int n_deals = 1000000;
+  int n_threads = 1;
+
+  if (argc > 1) {
+    n_deals = std::atoi(argv[1]);
+  }
+
+  if (argc > 2) {
+    n_threads = std::atoi(argv[2]);
+  }
+
+  printf("\nRunning %d threads of %d deals each...\n\n", n_threads, n_deals);
+
+  double* p0_total_value = new double[n_threads]();
+  double* p1_total_value = new double[n_threads]();
+
+  std::pair<int, double>** p0_action_and_value = new std::pair<int, double>*[n_threads]();
+  std::pair<int, double>** p1_action_and_value = new std::pair<int, double>*[n_threads]();
+
+  for (int i = 0; i < n_threads; i++) {
+    p0_action_and_value[i] = new std::pair<int, double>[Normal::N_OMAHA_HOLE_NORMALS]();
+    p1_action_and_value[i] = new std::pair<int, double>[Normal::N_OMAHA_HOLE_NORMALS]();
+  }
+
+  std::vector<std::thread> threads;
+
+  for (int i = 0; i < n_threads; i++) {
+    threads.push_back(std::thread (evaluate_hands, n_deals, &p0_total_value[i], p0_action_and_value[i], &p1_total_value[i], p1_action_and_value[i], 13*i));
+  }
+  
+  for (int i = 0; i < n_threads; i++) {
+    threads[i].join();
+  }
+
+  double total_p0_total_value = 0.0;
+  double total_p1_total_value = 0.0;
+
+  std::pair<int, double>* total_p0_action_and_value = new std::pair<int, double>[Normal::N_OMAHA_HOLE_NORMALS]();
+  std::pair<int, double>* total_p1_action_and_value = new std::pair<int, double>[Normal::N_OMAHA_HOLE_NORMALS]();
+  
+  for (int i = 0; i < n_threads; i++) {
+    total_p0_total_value += p0_total_value[i];
+    total_p1_total_value += p1_total_value[i];
+
+    std::pair<int, double>* p0_action_and_value_i = p0_action_and_value[i];
+    std::pair<int, double>* p1_action_and_value_i = p1_action_and_value[i];
+
+    for (int j = 0; j < (int)Normal::N_OMAHA_HOLE_NORMALS; j++) {
+      total_p0_action_and_value[j].first += p0_action_and_value_i[j].first;
+      total_p0_action_and_value[j].second += p0_action_and_value_i[j].second;
+
+      total_p1_action_and_value[j].first += p1_action_and_value_i[j].first;
+      total_p1_action_and_value[j].second += p1_action_and_value_i[j].second;
+    }
+  }
+  // if (0) for(int deal_no = 0; deal_no < N_DEALS; deal_no++) {
+  //   auto cards = dealer.deal(4+4+3+1+1);
+
+  //   auto p0_hole = std::make_tuple(CardT(cards[0+0]), CardT(cards[0+1]), CardT(cards[0+2]), CardT(cards[0+3]));
+  //   auto p1_hole = std::make_tuple(CardT(cards[4+0]), CardT(cards[4+1]), CardT(cards[4+2]), CardT(cards[4+3]));
+
+  //   int p0_hole_normal_index = Normal::omaha_hole_normal_index(std::get<0>(p0_hole), std::get<1>(p0_hole), std::get<2>(p0_hole), std::get<3>(p0_hole));
+  //   assert(0 <= p0_hole_normal_index && (std::size_t)p0_hole_normal_index < Poker::Normal::N_OMAHA_HOLE_NORMALS);
+  //   int p1_hole_normal_index = Normal::omaha_hole_normal_index(std::get<0>(p1_hole), std::get<1>(p1_hole), std::get<2>(p1_hole), std::get<3>(p1_hole));
+  //   assert(0 <= p1_hole_normal_index && (std::size_t)p1_hole_normal_index < Poker::Normal::N_OMAHA_HOLE_NORMALS);
+
+  //   auto flop = std::make_tuple(CardT(cards[4*2]), CardT(cards[4*2 + 1]), CardT(cards[4*2 + 2]));
+  //   auto turn = CardT(cards[4*2 + 3]);
+  //   auto river = CardT(cards[4*2 + 4]);
+
+  //   auto p0_hand_eval = HandEval::eval_hand_omaha(p0_hole, flop, turn, river);
+  //   auto p1_hand_eval = HandEval::eval_hand_omaha(p1_hole, flop, turn, river);
+
+  //   double p0_hand_value = 0.0;
+  //   double p1_hand_value = 0.0;
+    
+  //   if(p0_hand_eval > p1_hand_eval) {
+  //     p0_hand_value = 1.0;
+  //     p1_hand_value = -1.0;
+  //   } else if(p1_hand_eval > p0_hand_eval) {
+  //     p0_hand_value = -1.0;
+  //     p1_hand_value = 1.0;
+  //   }
+
+  //   p0_total_value += p0_hand_value;
+  //   p1_total_value += p1_hand_value;
+
+  //   add_action_and_value(p0_action_and_value, p0_hole_normal_index, p0_hand_value);
+  //   add_action_and_value(p1_action_and_value, p1_hole_normal_index, p1_hand_value);
+  // }
+
+  int total_n_deals = n_threads*n_deals;
+
+  printf("%d deals / p0 EV %+12.10lf / p1 EV %+12.10lf\n", total_n_deals, total_p0_total_value/total_n_deals, total_p1_total_value/total_n_deals);
 
   printf("\nPlayer 0:\n\n");
-  dump_action_and_value(p0_action_and_value, "  ");
+  dump_action_and_value(total_p0_action_and_value, "  ");
   printf("\n\nPlayer 1:\n\n");
-  dump_action_and_value(p1_action_and_value, "  ");
+  dump_action_and_value(total_p1_action_and_value, "  ");
 
   printf("\n\n");
 }
